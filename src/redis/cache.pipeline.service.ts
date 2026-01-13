f24aebfa8dcebd9b02d9416b62f0f22f6af00c07
tanvirAhmed73 <tanvir.073.ahmed@gmail.com>
2026-01-13 18:32:31 +0600

    refactor code

--- src/redis/cache.pipeline.service.ts
+++ src/redis/cache.pipeline.service.ts
@@ -1,9 +1,17 @@
 import { getRedisConnection } from "../config/redis.config";
+import { AppError } from "../utlis/appError";
+import logger from "../utlis/logger";
 
 export const batchSet = async (items: {key:string, value:any, ttl:number}[]) =>{
-    const pipeline = getRedisConnection().multi();
-    items.forEach(({key, value, ttl}) => {
-        pipeline.set(key, JSON.stringify(value), "EX", ttl);
-    });
-    await pipeline.exec();
+    try {
+        const pipeline = getRedisConnection().multi();
+        items.forEach(({key, value, ttl}) => {
+            pipeline.set(key, JSON.stringify(value), "EX", ttl);
+        });
+        await pipeline.exec();
+    } catch (error) {
+        logger.error("Error in batchSet", error);
+        throw new AppError("Failed to set cache in batch", 500);
+    }
+    
 }
--- src/redis/cache.service.ts
+++ src/redis/cache.service.ts
@@ -1,19 +1,41 @@
 import { getRedisConnection, redisConnection } from "../config/redis.config"
+import { AppError } from "../utlis/appError";
+import logger from "../utlis/logger";
 
 
 export const getCache = async (key:string) =>{
-    const data = await getRedisConnection().get(key);
-    return data? JSON.parse(data) : null;
+    try {
+        const data = await getRedisConnection().get(key);
+        return data? JSON.parse(data) : null;
+    } catch (error) {
+        logger.error("Error in getCache", error);
+        throw new AppError("Failed to get cache", 500);
+    }
 }
 
 export const setCachewithTTL = async (key:string, value:any, ttl:number) =>{
-    await getRedisConnection().set(key, JSON.stringify(value), "EX", ttl);
+    try {
+        await getRedisConnection().set(key, JSON.stringify(value), "EX", ttl);
+    } catch (error) {
+        logger.error("Error in setCachewithTTL", error);
+        throw new AppError("Failed to set cache with TTL", 500);
+    }
 }
 
 export const setCacheWithoutTTL = async (key:string, value:any) => {
-    await redisConnection().set(key, value);
+    try {
+        await redisConnection().set(key, value);
+    } catch (error) {
+        logger.error("Error in setCacheWithoutTTL", error);
+        throw new AppError("Failed to set cache without TTL", 500);
+    }
 }
 
 export const deleteCache = async (key:string)=>{
-    await redisConnection().del(key);
+    try {
+        await redisConnection().del(key);
+    } catch (error) {
+        logger.error("Error in deleteCache", error);
+        throw new AppError("Failed to delete cache", 500);
+    }
 }
